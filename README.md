ed73005bed85def18fd6214e36a988fab6dfb548
Rahul Batra <rahulbatra065@outlook.com>
2018-04-29 17:51:20 +0530

    ok bye repo
    hello moto

--- README.md
+++
@@ -1,4 +0,0 @@
-# nodedps.github.io
-Betanode website
-This website is the repository for the betanode website please tread with caution
--Rahul Batra
--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-var main= function(){
-	$('.scroll-but').click(function(){
-		$('window,body').animate({
-			scrollTop: $('.about').offset().top
-		},800);
-	});
-}
-$(document).ready(main);
